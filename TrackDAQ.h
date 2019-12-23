f34b0b272b52974be877391a187f11ce1c96743a
Jeff Williams <jeff@cnerds.net>
2019-12-23 10:23:50 -0500

    missing

---
+++ TrackDAQ.h
@@ -0,0 +1,37 @@
+//#define BNO055 1
+//#define EM7180_LIB 1
+#define EM7180 1
+
+#define NOSTOP I2C_NOSTOP
+
+#define RPM_ENGINE_PIN     21  // Engine RPM
+#define RPM_FWHEEL_PIN     5 // Front Wheel RPM
+#define RPM_RWHEEL_PIN     6 // Rear Wheel RPM
+
+#define DIGITAL_FBRAKE_PIN    24  // Front Brake Switch
+#define DIGITAL_NEUTRAL_PIN    25  // Neutral Switch
+
+#define ENABLE_PIN      29
+#define INTERNAL_LED    13
+#define ENABLE_LED      30	//30 / 8
+#define RGB_LED			0	//0 / 1
+
+#define LED_RED    			0x00FF00
+#define LED_GREEN  			0xFF0000
+#define LED_BLUE   			0x0000FF
+#define LED_YELLOW 			0xFFFF00
+#define LED_PINK   			0xFF1088
+#define LED_ORANGE 			0xE05800
+#define LED_WHITE  			0xFFFFFF
+#define LED_LIGHT_RED  		0x001600
+#define LED_LIGHT_GREEN		0x160000
+#define LED_LIGHT_BLUE 		0x000016
+#define LED_LIGHT_YELLOW	0x101400
+#define LED_LIGHT_PINK 		0x120009
+#define LED_LIGHT_ORANGE	0x100400
+#define LED_LIGHT_WHITE		0x101010
+#define LED_OFF    			0x000000
+
+
+#define BUF_SIZE 4
+
